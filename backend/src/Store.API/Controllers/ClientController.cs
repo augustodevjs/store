2e1620d7ed796c8ce45062842099cd6b414fd6af
augusto <augustosaaude@gmail.com>
2023-10-03 20:12:02 -0300

    feat: create initial controller of client and product

---
+++ backend/src/Store.API/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Application.Notifications;
+
+namespace Store.API.Controllers;
+
+[Route("client")]
+public class ClientController : MainController
+{
+    public ClientController(INotificator notificator) : base(notificator)
+    {
+    }
+}
---
+++ backend/src/Store.API/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Application.Notifications;
+
+namespace Store.API.Controllers;
+
+[Route("product")]
+
+public class ProductController : MainController
+{
+    public ProductController(INotificator notificator) : base(notificator)
+    {
+    }
+}
